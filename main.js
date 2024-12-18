0dc94a1e3378493a1cab4c90de616f7be433a4de
pixel <alex.pixelwinner@gmail.com>
2024-12-18 21:53:54 +0200

    Small fix

--- main.js
+++ main.js
@@ -5,9 +5,6 @@ let surface;                    // A surface model
 let shProgram;                  // A shader program
 let spaceball;                  // A SimpleRotator object that lets the user rotate the view by mouse.
 
-function deg2rad(angle) {
-    return angle * Math.PI / 180;
-}
 
 // Constructor
 function Model(name) {
