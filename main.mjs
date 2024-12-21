924711d317e3e728db948e7dd6b40e972931f4ac
pixel <alex.pixelwinner@gmail.com>
2024-12-21 18:46:40 +0200

    Fix

--- main.mjs
+++ main.mjs
@@ -122,10 +122,8 @@ function init() {
         return;
     }
 
-    initGL();
-
     try {
-        
+        initGL();
     } catch (e) {
         document.getElementById("canvas-holder").innerHTML =
             "<p>Sorry, could not initialize the WebGL graphics context: " + e + "</p>";
